016e25855bade142e53d2ef985b81a9c02dfd4f2
Dominique Hazael-Massieux <dom@w3.org>
2014-02-11 08:56:17 +0100

    add per-WG label to pull requests

--- test/labels.js
+++ test/labels.js
@@ -5,6 +5,15 @@ suite('Test github labels abstraction', function() {
     test('getLabelsFromFiles must return root dir of filename as label', function() {
         assert.equal("foo", label.getLabelsFromFiles([{filename: "foo/bar/baz"}])[0]);
     });
+
+    test('getLabelsFromSpecs must return dap and webrtc for mediacapture-streams files', function(done) {
+	label.getLabelsFromSpecs(["mediacapture-streams"]).then(function (labels) { assert.deepEqual(["wg:dap", "wg:webrtc", "mediacapture-streams"], labels[0].value); done()}).fail(done);
+    });
+
+    test('getLabelsFromIssue must return dap, webrtc and mediacapture-streams for pull request 160', function(done) {
+	label.getLabelsFromIssue(160).then(function (labels) { assert.deepEqual(["wg:dap", "wg:webrtc", "mediacapture-streams"], labels); done()}).fail(done);
+    });
+
     
     test('getLabelsFromFiles must return correct shortname of spec for directories which used to be spelled incorrectly', function() {
         assert.equal("shadow-dom", label.getLabelsFromFiles([{filename: "ShadowDOM"}])[0]);
