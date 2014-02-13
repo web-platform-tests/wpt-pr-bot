8bdcc6e9017455cee4901cd175a70d04ba50b081
Tobie <tobie.langel@gmail.com>
2014-02-13 10:09:18 +0100

    Don't include WG label in email subjects.

--- test/notify.js
+++ test/notify.js
@@ -16,6 +16,10 @@ suite('clarifyLabel', function() {
         assert.equal("old-tests", notify.clarifyLabel("old-tests"));
     });
     
+    test('clarifyLabel returns null for "wg:"-prefixed labels', function() {
+        assert.equal(null, notify.clarifyLabel("wg:foo"));
+    });
+	
     test('clarifyLabel suffixes any other string with "-tests"', function() {
         assert.equal("xxx-tests", notify.clarifyLabel("xxx"));
         assert.equal("123-tests", notify.clarifyLabel("123"));
