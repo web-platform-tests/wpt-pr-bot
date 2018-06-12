2f33959f86ed867ded7f655a303a7305a17facc4
Simon Pieters <zcorpan@gmail.com>
2018-06-12 19:59:20 -0400

    Rename label to status:needs-reviewers and rename ownersExcludingAuthor

--- test/get-reviewers.js
+++ test/get-reviewers.js
@@ -6,47 +6,47 @@ suite('Test get-reviewers', function() {
         assert.deepEqual(["bar", "foobar"], getReviewers({
             author: { login: "foo" },
             reviewers: [],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('getReviewers for simple, initial case, with pre-existing reviewers', function() {
         assert.deepEqual(["bar", "foobar"], getReviewers({
             author: { login: "foo" },
             reviewers: ["extra"],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('getReviewers for simple, initial case, with pre-existing reviewer that is an owner', function() {
         assert.deepEqual(["foobar"], getReviewers({
             author: { login: "foo" },
             reviewers: ["bar"],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('getReviewers for simple case, follow-up', function() {
         assert.deepEqual([], getReviewers({
             author: { login: "foo" },
             reviewers: ["foobar", "bar"],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('getReviewers for simple case, follow-up, with manually added reviewers', function() {
         assert.deepEqual([], getReviewers({
             author: { login: "foo" },
             reviewers: ["foobar", "bar", "extra"],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
@@ -58,27 +58,27 @@ suite('Test get-reviewers w/ root', function() {
         assert.deepEqual(["bar", "foobar", "jgraham"], getReviewers({
             author: { login: "foo" },
             reviewers: [],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: true,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('getReviewers for simple, initial case, with pre-existing reviewer that is a root owner', function() {
         assert.deepEqual(["bar", "foobar"], getReviewers({
             author: { login: "foo" },
             reviewers: ["jgraham"],
-            ownersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "bar" }, { login: "foobar" }],
             isRoot: true,
             rootReviewers: ["jgraham"]
         }).sort());
     });
-    
+
     test('rootReviewer is also owner', function() {
         assert.deepEqual(["foobar", "jgraham"], getReviewers({
             author: { login: "foo" },
             reviewers: [],
-            ownersExcludingAuthor: [{ login: "jgraham" }, { login: "foobar" }],
+            reviewersExcludingAuthor: [{ login: "jgraham" }, { login: "foobar" }],
             isRoot: false,
             rootReviewers: ["jgraham"]
         }).sort());
