a56b66951ab92ab1e4594f47d05bc97b8b7f20a1
Mike Pennisi <mike@mikepennisi.com>
2018-12-03 19:16:35 -0500

    Lint project source code

--- test/setup.js
+++ test/setup.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 var replay = require('replay');
 
-replay.fixtures = path.join(__dirname, 'fixtures')
+replay.fixtures = path.join(__dirname, 'fixtures');
 
 // Remove the "Authorization" header so the fixture data can be used in the
 // absence of a valid API token.
