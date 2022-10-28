1aad433ba884e3b43c33796748160e3149bd5a82
James C Scott III <jcscottiii@users.noreply.github.com>
2022-10-28 15:33:40 -0400

    Use Google Cloud Secret Manager to fill in secrets (#217)
    * Use Google Cloud Secret Manager to fill in secrets
    
    Fixes #213
    
    Code changes:
    - Repurpose create-secrets.js to read secrets on startup from secret
      manager. Previously, the secrets were bundled in the package uploaded
    to app engine. That means anyone downloading the bundle would get the
    secrets.
    - Because loadSecrets is now async, add promise chaining to index.js so that
      the secrets are loaded first before the app starts to prevent race
    conditions. Added some parts into functions to make main() more
    readable. The code moved into functions is unchanged
    
    * Fix variable names and add jsdoc to new functions

--- create-secrets.js
+++ create-secrets.js
@@ -1,22 +1,35 @@
 // istanbul ignore file
 
-'use strict'
+'use strict';
 
-const {Datastore} = require('@google-cloud/datastore');
-const fs = require('fs');
+const {SecretManagerServiceClient} = require('@google-cloud/secret-manager');
+const client = new SecretManagerServiceClient();
+const SECRET_PREFIX = 'projects/wpt-pr-bot/secrets/';
+const SECRET_SUFFIX = '/versions/latest';
 
+/**
+ * fetchSecret retrieves the latest version of the secret from secret manager.
+ * @param {string} tokenName The name of the secret in Secret Manager
+ * @return {string} The sensitive value stored in Secret Manager.
+ */
 async function fetchSecret(tokenName) {
-    const datastore = new Datastore({projectId: 'wpt-pr-bot'});
-    const key = datastore.key(['Token', tokenName]);
-    let entity = await datastore.get(key);
-    return entity[0].Secret;
+    const [version] = await client.accessSecretVersion({
+        name: SECRET_PREFIX + tokenName + SECRET_SUFFIX,
+    });
+    return version.payload.data.toString();
 }
 
-(async () => {
+/**
+ * loadSecrets retrieves all the secrets needed for the program
+ * @return {object} The object with all of the secrets
+ */
+async function loadSecrets() {
     const secrets = {
         webhookSecret: await fetchSecret('github-webhook-secret'),
         githubToken: await fetchSecret('wpt-pr-bot-github-token'),
         bugsWebkit: await fetchSecret('wpt-pr-bot-bugswebkit-token'),
     };
-    await fs.promises.writeFile('secrets.json', JSON.stringify(secrets), 'utf8');
-})().catch((reason) => { console.error(reason); process.exit(1) });
+    return secrets;
+}
+
+exports.loadSecrets = loadSecrets;
